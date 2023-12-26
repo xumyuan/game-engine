580adf40f1bcaceb8714bab84267e54fe99c9480
xumy <947656035@qq.com>
2023-12-26 10:46:15 +0800

    添加日志功能

--- game_engine/src/Engine.h
+++ game_engine/src/Engine.h
@@ -1,6 +1,8 @@
 #pragma once
 
-#include <stdio.h>
+
 #include "Engine/Application.h"
+
+#include "Engine/Log.h"
 
 #include "Engine/EntryPoint.h"
--- game_engine/src/Engine/EntryPoint.h
+++ game_engine/src/Engine/EntryPoint.h
@@ -5,6 +5,13 @@
 extern Engine::Application* Engine::CreateApplication();
 
 int main(int argc, char** argv) {
+
+	Engine::Log::Init();
+
+	EG_CORE_WARN("Initialized Log!");
+	int a = 5;
+	EG_INFO("Hello!Var={0}",a);
+
 	auto app = Engine::CreateApplication();
 	app->Run();
 	delete app;
